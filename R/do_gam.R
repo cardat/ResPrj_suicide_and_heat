ef7d0d7c2acf093565477dd01365489494e8f035
Lucas Hertzog <291828H@curtin.edu.au>
2024-02-12 17:14:08 +0800

    plot

--- R/do_gam.R
+++ R/do_gam.R
@@ -2,7 +2,8 @@ do_gam <- function(
     anomaly
 ){
 gam <- gam(
-  deaths ~ s(tmax_anomaly) + # s(tmax) the s specifies a non-linear(smoothed relationship between response and predictor tmax_anomaly)
+  deaths ~ 
+    s(tmax_anomaly) + # s(tmax) the s specifies a non-linear(smoothed relationship between response and predictor tmax_anomaly)
     age_group * sex * ns(year,3) + # interaction between agegrp, sex, and natural spline on year with 3 basis functions
     gcc + # ctegorical
     s(month, k=3, fx=T, bs = 'cc') + # another smooth - effect of month. 4 basis functions, TRUE degree of freedom, bs is cyclic cubic spline - useful for cyclical like m and yr.
@@ -21,14 +22,16 @@ par(mar = c(4, 4, 1, 1), # c(bottom, left, top, right)
     las = 1,
     cex.axis = 0.8,
     cex.lab = 1)
+
 plot(gam, select = 2,
      shade = TRUE, shade.col="grey", col="black", lwd=2, seWithMean = TRUE,
-     ylab = 'log Relative Risk',xlab='Month')
+     ylab = 'log Relative Risk',xlab='Month', ylim = c(-0.1, 0.1))
 abline(h=0, lty=2, col="black")
 abline(v=9, lty=2, col="black") # September
 abline(v=11, lty=2, col="black") # November
 # Place the text at a higher position with increased font size (cex)
 text(x = 10, y = -0.025, labels = "Spring", col = "black")  
+
 dev.off()
 
 return(gam)
--- R/do_plot_temp.R
+++ R/do_plot_temp.R
@@ -2,6 +2,7 @@ do_plot_temp <- function(
     dat_temp
 ){
   
+library(RColorBrewer)
 
 foo <- dat_temp[year %between% c(1950, 2019)]
 foo[, tmax_anomaly := tmax - monthly_tmax_avg]
@@ -19,12 +20,13 @@ breaks_years <- seq(from=min(df$year), to=max(df$year), by=5)
 labels_years <- as.character(breaks_years)
 
 png_filename <- "figures_and_tables/fig_temp.png"
-png(png_filename, res=200, width=1800, height=900)
+png(png_filename, res=200, width=1800, height=500)
 
 ggplot_object <- ggplot(df, aes(x=factor(year), y=1, fill=mean_tmax_anomaly)) + 
   geom_tile(color="white") + 
   scale_fill_gradientn(
-    colors=rev(heat.colors(100)), 
+    # colors=rev(heat.colors(100)),
+    colors=brewer.pal(9, "YlOrRd"),
     name="", 
     limits=c(min_anomaly, max_anomaly),
     breaks=c(min_anomaly, max_anomaly), 
@@ -40,7 +42,9 @@ ggplot_object <- ggplot(df, aes(x=factor(year), y=1, fill=mean_tmax_anomaly)) +
     axis.title.y = element_blank(),
     legend.title = element_text(size=0.5),
     plot.title = element_text(hjust=0.5),
-    legend.key.height = unit(0.5, "cm")  
+    legend.key.height = unit(0.5, "cm") ,
+    panel.grid.major = element_blank(), 
+    panel.grid.minor = element_blank()
   ) +
   guides(
     fill = guide_colorbar(
@@ -51,8 +55,8 @@ ggplot_object <- ggplot(df, aes(x=factor(year), y=1, fill=mean_tmax_anomaly)) +
       )
   ) +
   scale_x_discrete(breaks = breaks_years, labels = labels_years) +
-  geom_segment(aes(x = factor(2000), xend = factor(2019), y = 1.5, yend = 1.5), color = "deepskyblue3", size = 3) +
-  annotate("text", x = factor(2010), y = 1.56, label = "Study period", size = 3.5, angle = 0, hjust = 0.5, vjust = 1)
+  geom_segment(aes(x = factor(2000), xend = factor(2019), y = 1.51, yend = 1.51), color = "black", size = 1) +
+  annotate("text", x = factor(2010), y = 1.61, label = "Study period", size = 3, angle = 0, hjust = 0.8, vjust = 1)
 
 # Display the plot
 print(ggplot_object)
